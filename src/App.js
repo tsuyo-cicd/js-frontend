9d4f056b0fefbdef205caa51aa9033e3ddbbcaed
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2020-06-15 19:35:14 +0900

    modify Apps

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    contacts: [],
+    quote: ""
+  };
+
+  componentDidMount() {
+    const headers = { 'Content-Type': 'application/json' }
+
+    fetch("http://BACKAPP:80/quote", {headers})
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ quote: data })
+    })
+    .catch(console.log)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="App-body">
+          <table>
+            <tbody>
+              <tr>
+                <td>{this.state.quote.quote}</td>
+                <td className="author">{this.state.quote.author}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>      
+    );
+  }
 }
 
 export default App;
